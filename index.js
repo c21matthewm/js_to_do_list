ade24e9fc631742af5db55f84ff9727ddedf09a0
ella <96279971+hiaytus@users.noreply.github.com>
2023-10-03 08:08:59 -0700

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 console.log('My code is running');
 let toDoArr = [];
-const addTask (task) => {
-    toDoArr.push(task);
+// const addTask (task) => {
+    // toDoArr.push(task);
+let newItem = "";
+document.querySelector("#submitBtn").addEventListener("click", () => {
+    newItem = document.getElementById("toDoInput").value;
+    if (newItem) {
+        toDoArr.push(newItem);
+        document.getElementById("toDoInput").value = '';
+        console.log(toDoArr);
+    }
+})
     // write to HTML
-}
+//}
+
