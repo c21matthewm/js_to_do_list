4ae2226bdafc5444569dd87cf873897d411c6ed8
ella <96279971+hiaytus@users.noreply.github.com>
2023-10-03 08:57:10 -0700

    testing

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 console.log('My code is running');
 let toDoArr = [];
-// const addTask (task) => {
-    // toDoArr.push(task);
-let newItem = "";
+const addTask = (task) => {
+    toDoArr.push(task);
+    // console.log('first' + toDoArr);
+}
+
+
+let listElement = document.getElementById('toDoList');
 document.querySelector("#submitBtn").addEventListener("click", () => {
-    newItem = document.getElementById("toDoInput").value;
-    if (newItem) {
-        toDoArr.push(newItem);
-        document.getElementById("toDoInput").value = '';
-        console.log(toDoArr);
-    }
+    let newItem = document.getElementById("toDoInput").value;
+    addTask(newItem);
+    console.log('array' + toDoArr);
+   // console.log('NEWITEM' + newItem);
+    // console.log('middle' + toDoArr);
+    // if (newItem) {
+    //     addTask(newItem);
+    //     // console.log('if' + newItem);
+    //     // toDoArr.forEach((task) => { 
+    //     //     listElement.innerHTML = `<li>${task}</li>`;
+    //     // })
+    //     // document.getElementById("toDoInput").value = '';
+    // }
 })
     // write to HTML
-//}
 
