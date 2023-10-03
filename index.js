0637c8e96d5aad64980ec3a7aa1030aaed4693f0
ella <96279971+hiaytus@users.noreply.github.com>
2023-10-03 14:36:49 -0700

    commit

--- index.js
+++ index.js
@@ -5,16 +5,59 @@ let listElement = document.getElementById('toDoList');
 
 const addTask = (task) => {
     toDoArr.push(task);
+    localStorage.setItem('toDoArr',JSON.stringify(toDoArr));
     console.log('Arr: ' + toDoArr);
-    // localStorage.setItem('toDoArr',JSON.stringify(toDoArr));
 }
 
-// toDoArr.forEach((item) => {
-//     addTaskToDOM(item);
-// });
+if (toDoArr) {
+    window.onload = () => {
+        toDoArr.forEach((element) => {
+            let newItem = element;
+            const newNode = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            const textNode = document.createTextNode(newItem);
+            
+            newNode.appendChild(checkbox);
+            newNode.appendChild(textNode);
+            listElement.insertBefore(newNode, listElement.children[0])
+
+            checkbox.addEventListener('change', () => {
+            if (checkbox.checked){
+                newNode.style.textDecoration = 'line-through';
+            } else {
+                newNode.style.textDecoration = 'none';
+            }
+        });
+
+        })
+}
+}
+
+document.querySelector("#clear").addEventListener("click", () => {
+    let testVar = document.querySelectorAll("input[type='checkbox']:checked");
+    testVar.forEach((element, index) => {
+        toDoArr.splice(index, 1);
+        element.parentElement.remove();
+        localStorage.setItem('toDoArr',JSON.stringify(toDoArr));
+    })
+    console.log(testVar);
+
+})
+
+    // let arrayTest = [];
+    // arrayTest = testVar.toArray();
+    // console.log(arrayTest);
+    // toDoArr.map((element) => {
+    // })
+    // if (checkbox.checked){
+    //     newNode.style.textDecoration = 'line-through';
+    // } else {
+    //     newNode.style.textDecoration = 'none';
+    // }
 document.querySelector("#submitBtn").addEventListener("click", () => {
     const newItem = document.getElementById("toDoInput").value;
-    if(newItem) {
+    if(newItem != "") {
         addTask(newItem);
         const newNode = document.createElement('li');
         const checkbox = document.createElement('input');
