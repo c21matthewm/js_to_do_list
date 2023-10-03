e9b726d33216482a143b0016688955484f64d9d9
ella <96279971+hiaytus@users.noreply.github.com>
2023-10-03 12:37:28 -0700

    edit

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 console.log('My code is running');
 
-let toDoArr = [];
+let toDoArr = JSON.parse(localStorage.getItem('toDoArr')) || [];
 let listElement = document.getElementById('toDoList');
 
 const addTask = (task) => {
     toDoArr.push(task);
     console.log('Arr: ' + toDoArr);
+    // localStorage.setItem('toDoArr',JSON.stringify(toDoArr));
 }
 
+// toDoArr.forEach((item) => {
+//     addTaskToDOM(item);
+// });
 document.querySelector("#submitBtn").addEventListener("click", () => {
     const newItem = document.getElementById("toDoInput").value;
     if(newItem) {
@@ -28,7 +32,10 @@ document.querySelector("#submitBtn").addEventListener("click", () => {
         newNode.appendChild(checkbox);
         newNode.appendChild(textNode);
         listElement.insertBefore(newNode, listElement.children[0])
-        document.getElementById("toDoInput").value = ''; // re-added so it clears input box
+        document.getElementById("toDoInput").value = ''; 
+        
+        localStorage.setItem('newItem',JSON.stringify(newItem));
     }
+
 }
 )
