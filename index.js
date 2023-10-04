82a85e4f02dea4a17974f7c9496afb95260a4168
ella <96279971+hiaytus@users.noreply.github.com>
2023-10-03 17:45:45 -0700

    css styling

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ document.querySelector("#clear").addEventListener("click", () => {
     
     let checked = document.querySelectorAll("input[type='checkbox']:checked");
     checked.forEach((element, index) => {
-        // toDoArr.splice(index, 1);
     element.parentElement.remove();
     })
     
@@ -54,20 +53,21 @@ document.querySelector("#clear").addEventListener("click", () => {
         let testText = element.nextSibling.nodeValue;
         uncheckedArr.unshift(testText);
     })
-    
-    // toDoArr.filter((element) => )
-    console.log(toDoArr);
+    localStorage.setItem('toDoArr',JSON.stringify(uncheckedArr))
+})
+    // toDoArr.splice(index, 1);
+    // // toDoArr.filter((element) => )
+    // console.log(toDoArr);
     // let uncheckedList = document.querySelectorAll("input[type=checkbox]:not(:checked)");
     // console.log('unchecked: ' + uncheckedList);
     // let unchecked = document.querySelectorAll("input[type=checkbox]:not(:checked)");
     // let toDoArr = Array.from(unchecked);
-    localStorage.setItem('toDoArr',JSON.stringify(uncheckedArr))
     // // Array.from(unchecked).forEach((element) => {
     // //     localStorage.setItem('toDoArr',JSON.stringify(toDoArr));
     // })
     
 
-})
+
 
 
 
